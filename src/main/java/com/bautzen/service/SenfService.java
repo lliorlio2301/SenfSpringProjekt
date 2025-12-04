cfc18c228ff431a18f18d2371dd128bd2d30b831
lliorlio2301 <jorge2301@outlook.es>
2025-12-04 22:04:52 +0100

    Konstruktor in SenfService angepasst

--- src/main/java/com/bautzen/service/SenfService.java
+++ src/main/java/com/bautzen/service/SenfService.java
@@ -18,17 +18,17 @@ public class SenfService {
 
     List<Gericht> gerichte;
 
-    public SenfService(List<Gericht> gerichte) {
-        this.gerichte = gerichte;
+    public SenfService() {
+        this.gerichte = new ArrayList<>();
     }
 
     public void erstelleListe() {
-        gerichte = new ArrayList<>(List.of(
+        gerichte = List.of(
             new Gericht("Bautzener Senfeier", 8.50, true),
             new Gericht("Sorbisches Hochzeitsessen", 14.90, false),
             new Gericht("Teichelmauke", 11.20, false),
             new Gericht("Quark mit Leinöl", 7.50, true)
-        ));
+        );
     }
 
     public List<Gericht> getGerichte() {
