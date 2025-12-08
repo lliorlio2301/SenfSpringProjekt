d686f4ba88fa8deaf4da1120dcb95632dd74681c
lliorlio2301 <jorge2301@outlook.es>
2025-12-08 08:19:58 +0100

    Getters, Setters und Konstruktor für die Klasse

---
+++ src/main/java/com/bautzen/senfservice/model/Turm.java
@@ -0,0 +1,41 @@
+package com.bautzen.senfservice.model;
+
+public class Turm {
+
+    private int id;
+    private String name;
+    private double hoehe;
+    private boolean besuchbar;
+    
+    public Turm(int id, String name, double hoehe, boolean besuchbar) {
+        this.id = id;
+        this.name = name;
+        this.hoehe = hoehe;
+        this.besuchbar = besuchbar;
+    }
+    
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public double getHoehe() {
+        return hoehe;
+    }
+    public void setHoehe(double hoehe) {
+        this.hoehe = hoehe;
+    }
+    public boolean isBesuchbar() {
+        return besuchbar;
+    }
+    public void setBesuchbar(boolean besuchbar) {
+        this.besuchbar = besuchbar;
+    }
+}
