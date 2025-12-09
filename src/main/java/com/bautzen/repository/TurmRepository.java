4f1c69e64a3095745e583db7a3c83c30bdbfbe02
lliorlio2301 <jorge2301@outlook.es>
2025-12-09 07:00:15 +0100

    Neue Klasse für die Kommunikation mit der DB

---
+++ src/main/java/com/bautzen/repository/TurmRepository.java
@@ -0,0 +1,20 @@
+package com.bautzen.repository;
+
+import com.bautzen.senfservice.model.Turm;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface TurmRepository extends JpaRepository<Turm, Integer> {
+    
+    // Spring zaubert hier automatisch SQL-Queries draus!
+    // Wir müssen nur den Methodennamen richtig wählen.
+    
+    // SELECT * FROM Turm WHERE besuchbar = ?
+    List<Turm> findByBesuchbar(boolean besuchbar);
+
+    // SELECT * FROM Turm WHERE hoehe > ?
+    List<Turm> findByHoeheGreaterThan(double minHoehe);
+}
