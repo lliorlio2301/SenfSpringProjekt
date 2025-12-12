c12b7587c005fe940d2770fa37d501ad3e3d0ff7
lliorlio2301 <jorge2301@outlook.es>
2025-12-12 13:55:58 +0100

    Klasse angepasst mit Text und dem dazugehörigen Turm

--- src/main/java/com/bautzen/senfservice/model/Bewertung.java
+++ src/main/java/com/bautzen/senfservice/model/Bewertung.java
@@ -1,28 +1,56 @@
 package com.bautzen.senfservice.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
 @Table
 public class Bewertung {
 
-    private int bewertung;
+    private String text;
+    private int sterne; //1-6
+
+    @ManyToOne
+    @JoinColumn(name="turm_id")
+    @JsonIgnore // WICHTIG: Verhindert eine Endlos-Schleife beim Laden (Turm -> Bewertung -> Turm -> ...)
+    private Turm turm;
 
     public Bewertung(){}
 
-    public Bewertung(int bewertung) {
-        this.bewertung = bewertung;
+    public Bewertung(String text, int bewertung, Turm turm) {
+        this.text = text;
+        this.turm = turm;
+        this.sterne = bewertung;
     }
 
-
-
-    public int getBewertung() {
-        return bewertung;
+    public int getSterne() {
+        return sterne;
     }
 
-    public void setBewertung(int bewertung) {
-        this.bewertung = bewertung;
+    public void setSterne(int bewertung) {
+        this.sterne = bewertung;
     }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Turm getTurm() {
+        return turm;
+    }
+
+    public void setTurm(Turm turm) {
+        this.turm = turm;
+    }
+
+    
 
 }
